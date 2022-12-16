3edf9b8a55bd4b4b69d0d563eea928582815566e
Violen58 <violen@narod.ru>
2022-12-16 14:40:04 +0300

    2. Переменные. [#172635 #371778]

---
+++ src/main/java/ru/job4j/Variables.java
@@ -0,0 +1,22 @@
+package ru.job4j;
+
+public class Variables {
+    public static void main(String[] args) {
+        byte mem = 1;
+        System.out.println(mem);
+        short size = 21180;
+        System.out.println(size);
+        int age = 9999999;
+        System.out.println(age);
+        long money = 149600000000L;
+        System.out.println(money);
+        float length = 1.85F;
+        System.out.println(length);
+        double distance = 100500.99;
+        System.out.println(distance);
+        boolean baby = true;
+        System.out.println(baby);
+        char gender = 'Y';
+        System.out.println(gender);
+    }
+}
