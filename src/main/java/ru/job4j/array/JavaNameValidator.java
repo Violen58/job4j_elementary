e952e26382299004d8181fbe82b02829a1733178
Violen58 <violen@narod.ru>
2023-03-20 19:53:41 +0300

    6.9. String - это массив [#504884]

--- src/main/java/ru/job4j/array/JavaNameValidator.java
+++ src/main/java/ru/job4j/array/JavaNameValidator.java
@@ -8,24 +8,26 @@ public class JavaNameValidator {
         }
         char[] array = name.toCharArray();
         int charCode = name.codePointAt(0);
-        if (!isSpecialSymbol(charCode)
-                && !Character.isDigit(charCode)
-                && !isUpperLatinLetter(charCode)) {
+        if (isSpecialSymbol(charCode)
+                || Character.isDigit(charCode)
+                || isUpperLatinLetter(charCode)) {
+            return false;
+        } else {
             for (int i = 1; i < array.length; i++) {
                 charCode = name.codePointAt(i);
-                if (isSpecialSymbol(charCode)
-                        || isUpperLatinLetter(charCode)
-                        || isLowerLatinLetter(charCode)
-                        || Character.isDigit(charCode)) {
-                    return true;
+                if (!isSpecialSymbol(charCode)
+                        && !isUpperLatinLetter(charCode)
+                        && !isLowerLatinLetter(charCode)
+                        && !Character.isDigit(charCode)) {
+                    return false;
                 }
             }
         }
-        return false;
+        return true;
     }
 
     public static boolean isSpecialSymbol(int code) {
-        return (code == 65 || code == 95);
+        return (code == 36 || code == 95);
     }
 
     public static boolean isUpperLatinLetter(int code) {
